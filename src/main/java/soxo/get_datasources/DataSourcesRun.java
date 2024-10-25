8d0af15a8a1c551386469349421fada21965cbf8
Pblues <lungbaphe772003@gmail.com>
2024-10-25 21:31:17 +0700

    xo so ca nuoc Viet Nam

--- src/main/java/soxo/get_datasources/DataSourcesRun.java
+++ src/main/java/soxo/get_datasources/DataSourcesRun.java
@@ -2,9 +2,9 @@ package soxo.get_datasources;
 
 public class DataSourcesRun {
     public static void main(String[] args) {
-        String csvFilePath = "sources/XSMN.csv";
-        CSVHelper.updateCSVIfNewData(csvFilePath, WebCrawler.TPHCM_URL, "TP Hồ Chí Minh");
-        CSVHelper.updateCSVIfNewData(csvFilePath, WebCrawler.DONGTHAP_URL, "Đồng Tháp");
-        CSVHelper.updateCSVIfNewData(csvFilePath, WebCrawler.CAMAU_URL, "Cà Mau");
+        String csvFilePath = "sources/XSVN.csv";
+        WebCrawler.PROVINCE_URLS.forEach((province, url) -> {
+            CSVHelper.updateCSVIfNewData(csvFilePath, url, province);
+        });
     }
 }
--- src/main/java/soxo/get_datasources/WebCrawler.java
+++ src/main/java/soxo/get_datasources/WebCrawler.java
@@ -8,13 +8,52 @@ import org.jsoup.select.Elements;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WebCrawler {
-    public static final String TPHCM_URL = "https://www.xoso.net/getkqxs/tp-hcm";
-    public static final String DONGTHAP_URL = "https://www.xoso.net/getkqxs/dong-thap";
-    public static final String CAMAU_URL = "https://www.xoso.net/getkqxs/ca-mau";
+    public static final Map<String, String> PROVINCE_URLS = new HashMap<String, String>();
 
-    public static String[] getDataFromWebsite(String province, String xoSoUrl) {
+    static {
+        PROVINCE_URLS.put("Miền Bắc", "mien-bac");
+        PROVINCE_URLS.put("An Giang", "an-giang");
+        PROVINCE_URLS.put("Bạc Liêu", "bac-lieu");
+        PROVINCE_URLS.put("Bến Tre", "ben-tre");
+        PROVINCE_URLS.put("Bình Định", "binh-dinh");
+        PROVINCE_URLS.put("Bình Dương", "binh-duong");
+        PROVINCE_URLS.put("Bình Phước", "binh-phuoc");
+        PROVINCE_URLS.put("Bình Thuận", "binh-thuan");
+        PROVINCE_URLS.put("Cà Mau", "ca-mau");
+        PROVINCE_URLS.put("Cần Thơ", "can-tho");
+        PROVINCE_URLS.put("Đà Lạt", "da-lat");
+        PROVINCE_URLS.put("Đà Nẵng", "da-nang");
+        PROVINCE_URLS.put("Đắk Lắk", "dak-lak");
+        PROVINCE_URLS.put("Đắk Nông", "dak-nong");
+        PROVINCE_URLS.put("Đồng Nai", "dong-nai");
+        PROVINCE_URLS.put("Đồng Tháp", "dong-thap");
+        PROVINCE_URLS.put("Gia Lai", "gia-lai");
+        PROVINCE_URLS.put("Hậu Giang", "hau-giang");
+        PROVINCE_URLS.put("Khánh Hòa", "khanh-hoa");
+        PROVINCE_URLS.put("Kiên Giang", "kien-giang");
+        PROVINCE_URLS.put("Kon Tum", "kon-tum");
+        PROVINCE_URLS.put("Long An", "long-an");
+        PROVINCE_URLS.put("Ninh Thuận", "ninh-thuan");
+        PROVINCE_URLS.put("Phú Yên", "phu-yen");
+        PROVINCE_URLS.put("Quảng Bình", "quang-binh");
+        PROVINCE_URLS.put("Quảng Nam", "quang-nam");
+        PROVINCE_URLS.put("Quảng Ngãi", "quang-ngai");
+        PROVINCE_URLS.put("Quảng Trị", "quang-tri");
+        PROVINCE_URLS.put("Sóc Trăng", "soc-trang");
+        PROVINCE_URLS.put("Tây Ninh", "tay-ninh");
+        PROVINCE_URLS.put("Thừa Thiên Huế", "thua-thien-hue");
+        PROVINCE_URLS.put("Tiền Giang", "tien-giang");
+        PROVINCE_URLS.put("TP. HCM", "tp-hcm");
+        PROVINCE_URLS.put("Trà Vinh", "tra-vinh");
+        PROVINCE_URLS.put("Vĩnh Long", "vinh-long");
+        PROVINCE_URLS.put("Vũng Tàu", "vung-tau");
+    }
+
+    public static String[] getDataFromWebsite(String province, String provinceInURL) {
         String[] data = new String[11];
         try {
             LocalDate today = LocalDate.now();
@@ -22,7 +61,7 @@ public class WebCrawler {
             String formattedDate = today.format(formatter);
 
             // Crawl dữ liệu từ trang web
-            Document doc = Jsoup.connect(xoSoUrl + "/" + formattedDate + ".js").get();
+            Document doc = Jsoup.connect("https://www.xoso.net/getkqxs/" + provinceInURL + "/" + formattedDate + ".js").get();
             String[] targetClasses = {"giaidb", "giai1", "giai2", "giai3", "giai4", "giai5", "giai6", "giai7", "giai8"};
             data[0] = province; // Ví dụ: TPHCM, Đồng Tháp, v.v.
             int index = 1;
@@ -50,6 +89,6 @@ public class WebCrawler {
     }
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(WebCrawler.getDataFromWebsite("TPHCM_URL", TPHCM_URL)));
+        System.out.println(Arrays.toString(WebCrawler.getDataFromWebsite("TPHCM_URL", "tp-hcm")));
     }
 }
